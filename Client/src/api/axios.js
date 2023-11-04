21a45e5ed02272f94c462e65dd9df0ea8e0a3ff8
Johnny Vasil <jfvasil@gmail.com>
2023-11-04 18:17:22 -0400

    edited url

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://lake-wickaboag-backend-da96a41ce10c.herokuapp.com/'
 
 export default axios.create({
     baseURL: BASE_URL
