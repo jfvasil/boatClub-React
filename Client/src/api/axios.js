093aed83e8cd6ba05e419bf9affe492ddb7ebf0c
Johnny Vasil <jfvasil@gmail.com>
2023-11-05 09:27:50 -0500

    more cors troubleshooting

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://lake-wickaboag-backend-da96a41ce10c.herokuapp.com/'
+const BASE_URL = 'https://lake-wickaboag-backend-da96a41ce10c.herokuapp.com'
 // const BASE_URL = 'http://localhost:3000/'
 
 export default axios.create({
