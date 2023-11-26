e811e812afe08811d28fea9f5d5e47decd7ee66c
Johnny Vasil <jfvasil@gmail.com>
2023-11-26 18:03:36 -0500

    changed axios for new hosting

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://lake-wickaboag-backend-da96a41ce10c.herokuapp.com'
+const BASE_URL = 'http://18.118.4.119:3000/'
 // const BASE_URL = 'http://localhost:3000/'
 
 export default axios.create({
--- Client/src/pages/Login.js
+++ Client/src/pages/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
 
     const navigate = useNavigate()
     const location = useLocation()
-    const from = location.state?.from?.pathname || "/home"
+    const from = location.state?.from?.pathname || "/"
 
     const userRef = useRef()
     const errRef = useRef()
