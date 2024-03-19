d55af26975856914a38eb083552d5e414f8192d1
Johnny Vasil <jfvasil@gmail.com>
2024-03-19 11:11:39 -0400

    added new officer

--- Client/src/components/mainComponents/ContactList.js
+++ Client/src/components/mainComponents/ContactList.js
@@ -85,6 +85,15 @@ const ContactList = () => {
         <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
           <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={boatIcon}></img>
           <div className="flex-grow">
+            <h2 className="text-gray-900 title-font font-medium">Mark Tuttle</h2>
+            <p className="text-gray-500">Treasurer</p>
+          </div>
+        </div>
+      </div>
+      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={boatIcon}></img>
+          <div className="flex-grow">
             <h2 className="text-gray-900 title-font font-medium">Ken Henneman, Bill Hissem, Gerry Nichols, Chris Dimarzio</h2>
             <p className="text-gray-500">Directors</p>
           </div>
